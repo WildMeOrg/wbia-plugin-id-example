6c977adc49eab9620936b5ba7307e04f0e2fb21a
Jason Parham <parham@wildme.org>
2019-06-17 15:08:11 -0700

    Initial Commit - Clone from https://github.com/WildbookOrg/ibeis_plugin_example

---
+++ ibeis_plugin_example/version.py
@@ -0,0 +1,8 @@
+
+# THIS FILE IS GENERATED FROM SETUP.PY
+version = '0.1.0.dev0'
+git_revision = '3e48057a0d20255e089927c95dac68ae58b75dfb'
+full_version = '%(version)s.%(git_revision)s' % {
+    'version': version,
+    'git_revision': git_revision,
+}
